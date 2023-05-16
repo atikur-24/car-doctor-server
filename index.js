935dfe94d3199be85fea1da466c3a4daf53fe53c
Atikur <atikurrahman7223@gmail.com>
2023-05-16 16:45:05 +0600

    data recive from client and send to database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,13 +26,35 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const serviceCollection = client.db('carDoctor').collection('services');
+    const database = client.db('carDoctor');
+    const serviceCollection = database.collection('services');
+    const OrdersCollection = database.collection('orders');
 
+    // services routes
     app.get('/services', async(req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     });
+
+    app.get('/services/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { service_id: 1, title: 1, price: 1, img: 1 }
+      }
+      const result = await serviceCollection.findOne(query, options)
+      res.send(result);
+    });
+
+    //orders
+    app.post('/checkout', async(req, res) => {
+      const orders = req.body;
+      const result = await OrdersCollection.insertOne(orders);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
